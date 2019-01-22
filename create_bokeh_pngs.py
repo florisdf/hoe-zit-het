347b4c2144fd2bd7059ae2c563b89a19be32b4f5
florisdf <floris.defeyter@kuleuven.be>
2019-01-22 21:27:47 +0100

    Add create_pngs

---
+++ create_bokeh_pngs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import json
+from pathlib import Path
+from bokeh.document import Document
+import bokeh.io
+from tqdm import tqdm
+import hashlib
+
+LAZY = True
+
+docs = {p: Document.from_json(json.load(p.open())['doc'])
+        for p in Path('content/lessen/').rglob('*/plt/*.json')}
+
+for p, doc in tqdm(docs.items()):
+    md5_file = p.parent / (p.stem + '.md5')
+    m = hashlib.md5()
+    m.update(p.read_bytes())
+    cur_md5sum = m.digest()
+
+    if not md5_file.exists():
+        md5_file.write_bytes(cur_md5sum)
+
+    prev_md5sum = md5_file.read_bytes()
+
+    if prev_md5sum != cur_md5sum or not LAZY:
+        md5_file.write_bytes(cur_md5sum)
+        fn = p.parent / f'{p.stem}.png'
+        fig = doc.roots[0]
+        fig.sizing_mode = 'fixed'
+        fig.toolbar_location = None
+        fig.outline_line_alpha = 0
+        bokeh.io.export_png(fig, fn, width=600, height=600)
