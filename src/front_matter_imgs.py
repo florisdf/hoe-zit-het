17d51fe52df1a50b17d4fea41768923a25464d60
florisdf <floris.defeyter@kuleuven.be>
2019-10-05 00:47:32 +0200

    Add script for generating front matter images

---
+++ src/front_matter_imgs.py
@@ -0,0 +1,19 @@
+import re
+from pathlib import Path
+
+lessons = [*Path('content/lessen/').rglob('*.md')]
+
+for lesson in lessons:
+    matches = re.finditer('{{% (img|svg) "([^"]*)" %}}', lesson.read_text())
+    imgs = [m.group(2).replace('.svg', '.png') for m in matches]
+
+    lines = lesson.read_text().split('\n')
+    idxs = [i for i, l in enumerate(lines) if re.match('images: \[.*\]', l)]
+    if len(idxs) == 1:
+        idx = idxs[0]
+        lines[idx] = f"images: {imgs}"
+    else:
+        idx = [i for i, l in enumerate(lines) if l == '---'][1]
+        lines.insert(idx, f"images: {imgs}")
+
+    lesson.write_text('\n'.join(lines))
