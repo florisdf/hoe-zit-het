f34559c601faacbb48975207daf910ec3aca9e7b
Floris De Feyter <floris@hoezithet.nu>
2020-07-19 16:44:43 +0200

    Move dash in wkhtmltopdf command to end

--- src/create_pdfs.py
+++ src/create_pdfs.py
@@ -9,9 +9,9 @@ articles = list(Path('content/lessen').rglob('*/index.md'))
 for p in tqdm(articles):
     url = Path(str(p).replace("content/", "public/", 1).replace("index.md", "index.html"))
     pdf_file = url.parent / ('-'.join([url.parent.name, url.parent.parent.name]).title() + '.pdf')
-    args = ['wkhtmltopdf', '-', '-T', '25mm', '-B', '25mm', '-R', '25mm', '-L',
+    args = ['wkhtmltopdf', '-T', '25mm', '-B', '25mm', '-R', '25mm', '-L',
             '25mm', '--no-stop-slow-scripts',
-            '--javascript-delay', '5000', '--viewport-size', '1920x1080',
+            '--javascript-delay', '5000', '--viewport-size', '1920x1080', '-',
             str(pdf_file)]
     child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE)
     child_proccess.stdin.write(url.read_bytes())
