d2b2d771cae11cba36a91e5e5a8eb6925d2c6ee2
Floris De Feyter <floris@hoezithet.nu>
2020-07-19 16:19:26 +0200

    Build pdfs straight to public folder

--- src/create_pdfs.py
+++ src/create_pdfs.py
@@ -1,48 +1,19 @@
 #!/usr/bin/env python
 from pathlib import Path
-from urllib.request import urlopen
-from urllib.error import HTTPError
 from tqdm import tqdm
 import logging
-from subprocess import run
-import hashlib
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--force',
-                    help='Create all pdfs, even if their '
-                    'source file didn\'t change.',
-                    action='store_true')
-args = parser.parse_args()
-LAZY = not args.force
+import subprocess
 
 articles = list(Path('content/lessen').rglob('*/index.md'))
 
-logging.info('Checking urls...')
 for p in tqdm(articles):
-    url = f'file://{Path(str(p).replace("content/", "public/", 1).replace("index.md", "index.html")).absolute()}'
-    print(url)
-    md5_file = p.parent / (p.stem + '.md5')
-    m = hashlib.md5()
-    m.update(p.read_bytes())
-    cur_md5sum = m.digest()
-
-    if not md5_file.exists():
-        md5_file.write_bytes(cur_md5sum)
-        prev_md5sum = None
-    else:
-        prev_md5sum = md5_file.read_bytes()
-
-    if prev_md5sum != cur_md5sum or not LAZY:
-        md5_file.write_bytes(cur_md5sum)
-        try:
-            url_elements = url.split('/')[1:]
-            print(*url_elements)
-            pdf_dir = Path('content/lessen/' + '/'.join(url_elements))
-            pdf_file = pdf_dir / ('-'.join(url_elements[-2:]).title() + '.pdf')
-            run(['wkhtmltopdf', '-T', '25mm', '-B', '25mm', '-R', '25mm', '-L',
-                 '25mm', url, '--no-stop-slow-scripts',
-                '--javascript-delay', '5000', '--viewport-size', '1920x1080',
-                 str(pdf_file)])
-        except HTTPError as e:
-            logging.info(f'{url} returned {e.code}')
+    url = Path(str(p).replace("content/", "public/", 1).replace("index.md", "index.html"))
+    pdf_file = url.parent / ('-'.join([url.parent.name, url.parent.parent.name]).title() + '.pdf')
+    args = ['wkhtmltopdf', '-', '-T', '25mm', '-B', '25mm', '-R', '25mm', '-L',
+            '25mm', '--no-stop-slow-scripts',
+            '--javascript-delay', '5000', '--viewport-size', '1920x1080',
+            str(pdf_file)]
+    child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE)
+    child_proccess.stdin.write(url.read_text())
+    child_proccess.communicate()
+    child_proccess.stdin.close()
