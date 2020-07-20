d419b0e8373963227fbb3ab63c7a27532b1e2e59
Floris De Feyter <floris@hoezithet.nu>
2020-07-20 08:40:36 +0200

    Add pdf links to lessons + fix image links + use bare lessons

--- src/create_pdfs.py
+++ src/create_pdfs.py
@@ -7,13 +7,22 @@ import subprocess
 articles = list(Path('content/lessen').rglob('*/index.md'))
 
 for p in tqdm(articles):
-    url = Path(str(p).replace("content/", "public/", 1).replace("index.md", "index.html"))
-    pdf_file = url.parent / ('-'.join([url.parent.name, url.parent.parent.name]).title() + '.pdf')
+    bare_html = Path(str(p).replace("content/", "public/bare/", 1).replace("index.md", "index.html"))
+    lesson_html = Path(str(bare_html).replace("public/bare/", "public/", 1))
+    pdf_file = lesson_html.parent / ('-'.join([lesson_html.parent.parent.name, lesson_html.parent.name]).title() + '.pdf')
     args = ['wkhtmltopdf', '-T', '25mm', '-B', '25mm', '-R', '25mm', '-L',
             '25mm', '--no-stop-slow-scripts',
             '--javascript-delay', '5000', '--viewport-size', '1920x1080', '-',
             str(pdf_file)]
     child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE)
-    child_proccess.stdin.write(url.read_bytes())
+    bare_content = bare_html.read_text()
+    # Replace absolute refs by relative refs
+    bare_content.replace('="/', '="public/')
+    child_proccess.stdin.write()
     child_proccess.communicate()
     child_proccess.stdin.close()
+   
+   # Link to correct pdf-file by replacing href pattern in lesson html file
+   pdf_link = str(pdf_file).replace('public/', '/', 1)
+   new_html_content = lesson_html.read_text().replace("##REPLACED_BY_CREATE_PDFS##", pdf_link)
+   lesson_html.write_text(new_html_content)
