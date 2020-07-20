560dbd997c38a6b5a42163ef5aef64da5fc1075f
florisdf <floris.defeyter@kuleuven.be>
2020-07-20 09:31:26 +0200

    Fix links to images etc. for wkhtmltopdf

--- src/create_pdfs.py
+++ src/create_pdfs.py
@@ -7,22 +7,28 @@ import subprocess
 articles = list(Path('content/lessen').rglob('*/index.md'))
 
 for p in tqdm(articles):
-    bare_html = Path(str(p).replace("content/", "public/bare/", 1).replace("index.md", "index.html"))
+    bare_html = (Path(str(p).replace("content/",
+                                    "public/bare/", 1)
+                      .replace("index.md", "index.html")))
     lesson_html = Path(str(bare_html).replace("public/bare/", "public/", 1))
-    pdf_file = lesson_html.parent / ('-'.join([lesson_html.parent.parent.name, lesson_html.parent.name]).title() + '.pdf')
+    pdf_file = (lesson_html.parent
+                / ('-'.join([lesson_html.parent.parent.name, 
+                             lesson_html.parent.name]).title() + '.pdf'))
     args = ['wkhtmltopdf', '-T', '25mm', '-B', '25mm', '-R', '25mm', '-L',
             '25mm', '--no-stop-slow-scripts',
             '--javascript-delay', '5000', '--viewport-size', '1920x1080', '-',
             str(pdf_file)]
     child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE)
-    bare_content = bare_html.read_text()
     # Replace absolute refs by relative refs
-    bare_content.replace('="/', '="public/')
-    child_proccess.stdin.write(bare_content.encode('utf-8'))
+    root_dir = Path('public').absolute()
+    child_proccess.stdin.write(bare_html.read_text()
+                               .replace('="/bare/', f'="{root_dir}/')
+                               .encode('utf-8'))
     child_proccess.communicate()
     child_proccess.stdin.close()
    
     # Link to correct pdf-file by replacing href pattern in lesson html file
     pdf_link = str(pdf_file).replace('public/', '/', 1)
-    new_html_content = lesson_html.read_text().replace("##REPLACED_BY_CREATE_PDFS##", pdf_link)
+    new_html_content = (lesson_html.read_text()
+                        .replace("##REPLACED_BY_CREATE_PDFS##", pdf_link))
     lesson_html.write_text(new_html_content)
