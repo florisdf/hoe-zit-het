c493ba4c24da30157f4a6ffbfcf67b00559e24a7
florisdf <floris.defeyter@kuleuven.be>
2020-07-21 13:33:15 +0200

    Add debugging code to check dirs in Travis-CI

--- src/create_pdfs.py
+++ src/create_pdfs.py
@@ -14,10 +14,8 @@ for p in tqdm(articles[4:5]):
     lesson_html = Path(str(bare_html).replace("public/bare/", "public/",
                                               1)).absolute()
 
-    logging.log(logging.WARNING, str(bare_html))
-    logging.log(logging.WARNING, 'Contents:')
-    logging.log(logging.WARNING, '\n'.join([str(p) for p in bare_html.parent.iterdir()])))
-
+    print(str(bare_html))
+    print('Contents:')
     pdf_file = (lesson_html.parent
                 / ('-'.join([lesson_html.parent.parent.name, 
                              lesson_html.parent.name]).title() + '.pdf'))
@@ -25,16 +23,16 @@ for p in tqdm(articles[4:5]):
             '25mm', '--no-stop-slow-scripts',
             '--javascript-delay', '5000', '--viewport-size', '1920x1080', '-',
             str(pdf_file)]
-    child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE)
+    # child_proccess = subprocess.Popen(args, stdin=subprocess.PIPE)
     # Replace absolute refs by relative refs
     root_dir = Path('public').absolute()
     html_content = bare_html.read_text()
     
-    for match in re.finditer(r'="/bare/[^"]*"', html_content):
-        src_path = match.group(0)
-        new_src_path = src_path.replace('="/bare/', f'="{root_dir}/')
-        logging.log(logging.WARNING, new_src_path)
-        logging.log(logging.WARNING, f'Exists? {Path(new_src_path).exists()}')
+    for match in re.finditer(r'="(/bare/[^"]*)"', html_content):
+        src_path = match.group(1)
+        new_src_path = src_path.replace('/bare/', f'{root_dir}/')
+        print(new_src_path)
+        print(f'Exists? {Path(new_src_path).exists()}')
                 
     # child_proccess.stdin.write(html _content
     #                            .replace('="/bare/', f'="{root_dir}/')
